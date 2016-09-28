fccde8c437adaf41535c0e1d6ff89db8980e71e9
Herman verschooten <Herman@verschooten.net>
2016-09-28 10:46:06 +0200

    Update phoenix

--- web/models/student.ex
+++ web/models/student.ex
@@ -35,7 +35,7 @@ defmodule Studay.Student do
     |> update_change(:firstname, &String.downcase/1)
     |> update_change(:telephone, fn(x) -> String.replace(x,~r/ |\./,"") end)
     |> validate_format(:email, ~r/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/)
-    |> validate_format(:telephone, ~r/^(\+32|+31|+33|+34|0)\d{9}$/)
+    |> validate_format(:telephone, ~r/^(\+32|\+31|\+33|\+34|0)\d{9}$/)
   end
 
   def sorted(query) do
